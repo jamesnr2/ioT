82f76b7279674016916247303f48158a0bd95e54
infraSVQ <jamesnr2@gmail.com>
2018-05-11 10:18:58 +0200

    Configuraciones locales
    Contiene la configuración local que usamos: wifi, host, etc.

---
+++ ESP8266/DoorOpen5/MyConfigLocal.h
@@ -0,0 +1,17 @@
+/* ARCHIVO "MyConfigLocal.h" 
+   Contiene la configuración local que usamos: wifi, host, etc.
+*/
+MyConfigLocal.h
+
+// Configuración del wifi que usemos
+const char* ssid = "MY_SSID";
+const char* password = "MI_CLAVE";
+
+// Configuración servidor1
+const char* host = "maker.ifttt.com";
+const char* apiKey = "KEY";
+const int httpPort = 80;
+
+// Configuración servidor2
+const int port = 80;
+const char* host2 = "192.168.1.1"; // ip or dns
