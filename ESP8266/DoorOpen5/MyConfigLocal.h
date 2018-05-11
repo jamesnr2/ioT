2a60ceca39c63efe6a6a8e013227b1f95672c787
infraSVQ <jamesnr2@gmail.com>
2018-05-11 10:24:58 +0200

    Parámetros completos para la llamada

--- ESP8266/DoorOpen5/MyConfigLocal.h
+++ ESP8266/DoorOpen5/MyConfigLocal.h
@@ -8,10 +8,14 @@ const char* ssid = "MY_SSID";
 const char* password = "MI_CLAVE";
 
 // Configuración servidor1
-const char* host = "maker.ifttt.com";
-const char* apiKey = "KEY";
-const int httpPort = 80;
+const char* host1 = "maker.ifttt.com";
+const int httpPort1 = 80;
+const char* apiKey1 = "KEY";
+String path1="";
+
 
 // Configuración servidor2
-const int port = 80;
 const char* host2 = "192.168.1.1"; // ip or dns
+const int httpPort2 = 80;
+const char* apiKey2 = "KEY";
+String path2="";
